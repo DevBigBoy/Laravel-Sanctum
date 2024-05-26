e16b90dee8525ca5ea4d112120fb1e31b5ae5de6
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-05-26 11:45:25 +0300

    working With Login functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,25 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\API\LoginUserRequest;
 use App\Http\Requests\API\storeUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login()
+    public function login(LoginUserRequest $request)
     {
+        $validated = $request->validated();
 
-        return 'This This my Login Method......';
+        $validated = $request->safe()->only(['email', 'password']);
+
+        if (!Auth::attempt($validated)) {
+            return $this->error('', 'Credentials do not match', 401);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+
+        return $this->success(
+            [
+                'user' => $user,
+                'token' => $user->createToken('Api Token of' . $user->name)->plainTextToken
+            ]
+        );
     }
 
     public function register(storeUserRequest $request)
     {
-        // $request->validated($request->all());
+        $request->validated($request->all());
 
         $user =  User::create([
             'name' => $request->name,
